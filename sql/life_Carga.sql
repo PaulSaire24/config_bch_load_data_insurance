dfa7d644cf750fa358532a68231ccb0be5ba6253
P029801 <jhon.quispe@bbva.com>
2023-09-01 10:47:15 -0500

    PERAR-24652 updated mod fee to dynamic life plan 02

--- sql/life_Carga.sql
+++ sql/life_Carga.sql
@@ -1,5 +1,10 @@
 update PISD.T_PISD_INSRNC_PRD_MODALITY
 set INSURANCE_COMPANY_CONTRACT_ID ='00110241780200526997'
-    where insurance_product_id = 130;
+    where insurance_product_id = 9;
+
+update PISD.T_PISD_SCHAN_INSRNC_MOD_FEE
+set policy_commission_per = 15
+where INSURANCE_PRODUCT_ID = 9
+     and insurance_modality_type='02';
 
 commit;
