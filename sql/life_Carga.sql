54f9826f8c19443816064a2d841e9b9aac3c6dfd
P029801 <jhon.quispe@bbva.com>
2023-09-29 17:48:13 -0500

    PERAR-24652 query to example

--- sql/life_Carga.sql
+++ sql/life_Carga.sql
@@ -1,10 +1,8 @@
-update PISD.T_PISD_INSRNC_PRD_MODALITY
-set INSURANCE_COMPANY_CONTRACT_ID ='00110241780200526997'
-    where insurance_product_id = 9;
+SET LINESIZE 3500;
+SET PAGESIZE 5000;
 
-update PISD.T_PISD_SCHAN_INSRNC_MOD_FEE
-set policy_commission_per = 15
-where INSURANCE_PRODUCT_ID = 9
-     and insurance_modality_type='02';
+
+select * from PISD.T_PISD_INSURANCE_PRODUCT
+WHERE INSURANCE_PRODUCT_ID = 9;
 
 commit;
