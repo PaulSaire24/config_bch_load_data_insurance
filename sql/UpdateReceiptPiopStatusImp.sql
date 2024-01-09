0a775e9c3d7c3579b0fc2abd4c66ad31e3b7132a
Isaac Duberli Mogrovejo Tucto <isaac.mogrovejo.contractor@bbva.com>
2024-01-08 19:39:04 -0500

    PERAR-25842: PREPARACION DE DATA PARA PRUEBAS: CASOS SUCCESS

---
+++ sql/UpdateReceiptPiopStatusImp.sql
@@ -0,0 +1,13 @@
+UPDATE piop.t_piop_receipt_payable
+SET
+    receipt_final_status_type = 'IMP',
+    openpay_commerce_trans_date = TO_DATE('2023-12-29', 'YYYY-MM-DD'),
+    commerce_receipt_status_type = 'REC',
+    collection_file_reception_date = TO_DATE('2023-12-29', 'YYYY-MM-DD'),
+    incidence_error_desc = 'no se pudo cobrar por falta de fondos'
+
+WHERE
+        product_contract_account_id IN ( '4580000500', '4580000600', '4580000700', '4580000800')
+  AND receipt_id = 2;
+
+COMMIT;
