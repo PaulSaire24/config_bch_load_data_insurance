36e082de02b45b768fdb3938d3980a0e03cf6e4a
Isaac Duberli Mogrovejo Tucto <isaac.mogrovejo.contractor@bbva.com>
2024-01-10 20:05:17 -0500

    PERAR-25842:Correccion query DataPruebaFailedPiop

--- sql/UpdateReceiptPiopStatusFact.sql
+++ sql/UpdateReceiptPiopStatusFact.sql
@@ -6,6 +6,6 @@ SET
     collection_file_reception_date = NULL,
     incidence_error_desc = NULL
 WHERE
-        product_contract_account_id IN ( '4580001900', '4580002000')
+    product_contract_account_id IN ('4580000900','4580001000','4580001100','4580001200','4580001300','4580001400','4580001500','4580001600','4580001900','4580002000')
 AND receipt_id = 2;
 COMMIT;
