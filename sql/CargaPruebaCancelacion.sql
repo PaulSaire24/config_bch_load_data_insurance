aecb4a67b4e6737be03ac992637050b68e29291b
Jun Naruse <jun.naruse@bbva.com>
2024-03-06 11:49:37 -0500

    [PARSRBVD-1517] Carga de datos para prueba de cancelacion

---
+++ sql/CargaPruebaCancelacion.sql
@@ -0,0 +1,436 @@
+-- Delete
+DELETE FROM PISD.T_PISD_INSURANCE_REQ_CNCL_MOV WHERE REQUEST_SEQUENCE_ID IN ('1', '2', '3', '4');
+DELETE FROM PISD.T_PISD_INSURANCE_REQUEST_CNCL WHERE REQUEST_SEQUENCE_ID IN ('1', '2', '3', '4');
+DELETE FROM PISD.T_PISD_INSRNC_CONTRACT_CNCL WHERE (INSURANCE_CONTRACT_ENTITY_ID || INSURANCE_CONTRACT_BRANCH_ID || INSRC_CONTRACT_INT_ACCOUNT_ID) IN ('001177944000000885', '001177944000001091', '400307894580132200', '400307894580132100');
+
+-- End of validaty, request date two days ago
+UPDATE
+    PISD.T_PISD_INSURANCE_CONTRACT
+SET
+    CONTRACT_STATUS_ID = 'PEN'
+WHERE
+    (
+        INSURANCE_CONTRACT_ENTITY_Id || INSURANCE_CONTRACT_BRANCH_ID || CONTRACT_FIRST_VERFN_DIGIT_ID || CONTRACT_SECOND_VERFN_DIGIT_ID || INSRC_CONTRACT_INT_ACCOUNT_ID
+    ) = '00117794504000000885';
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQUEST_CNCL (
+        REQUEST_SEQUENCE_ID,
+        INSURANCE_CONTRACT_ENTITY_ID,
+        INSURANCE_CONTRACT_BRANCH_ID,
+        INSRC_CONTRACT_INT_ACCOUNT_ID,
+        CONTRACT_FIRST_VERFN_DIGIT_ID,
+        CONTRACT_SECOND_VERFN_DIGIT_ID,
+        CHANNEL_ID,
+        CANCEL_BRANCH_ID,
+        REQUEST_CNCL_POLICY_DATE,
+        INSURANCE_PRODUCT_ID,
+        INSURANCE_MODALITY_TYPE,
+        PAYMENT_FREQUENCY_ID,
+        POLICY_ID,
+        COLECTIVE_CERTIFICATE_ID,
+        PREMIUM_AMOUNT,
+        PREMIUM_CURRENCY_ID,
+        CONTACT_EMAIL_DESC,
+        CUSTOMER_PHONE_DESC,
+        CUSTOMER_ID,
+        POLICY_ANNULATION_DATE,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE,
+        INSURANCE_CNCL_RETURNED_AMOUNT,
+        RET_AMOUNT_CURRENCY_ID,
+        INSRC_COMPANY_RETURNED_AMOUNT,
+        INSRC_CO_RTURN_AMOUNT_CCY_ID,
+        BBVA_RETURNED_AMOUNT,
+        BBVA_RTURN_AMOUNT_CURNCY_ID,
+        RL_ACCOUNT_ID,
+        REQUEST_STATUS_NAME,
+        REQUEST_TYPE
+    )
+VALUES
+    (
+        '1',
+        '0011',
+        '7794',
+        '4000000885',
+        '5',
+        '0',
+        'PC',
+        null,
+        TO_TIMESTAMP('2024-03-03', 'YYYY-MM-DD'),
+        '12',
+        null,
+        null,
+        '1300034100',
+        null,
+        null,
+        null,
+        'jose.sandoval.tirado.contractor@bbva.com',
+        '123456789',
+        '97788994',
+        TO_DATE('2024-03-31', 'YYYY-MM-DD'),
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        '4040123409876',
+        'END_OF_VALIDATY',
+        '000'
+    );
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQ_CNCL_MOV (
+        REQUEST_SEQUENCE_ID,
+        SEQ_MOV_NUMBER,
+        ADDITIONAL_DATA_DESC,
+        CONTRACT_STATUS_DATE,
+        CONTRACT_STATUS_ID,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE
+    )
+VALUES
+    (
+        '1',
+        1,
+        '10',
+        TO_DATE('2024-03-05', 'YYYY-MM-DD'),
+        '01',
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP
+    );
+
+-- PEN
+UPDATE
+    PISD.T_PISD_INSURANCE_CONTRACT
+SET
+    CONTRACT_STATUS_ID = 'PEN'
+WHERE
+    (
+        INSURANCE_CONTRACT_ENTITY_Id || INSURANCE_CONTRACT_BRANCH_ID || CONTRACT_FIRST_VERFN_DIGIT_ID || CONTRACT_SECOND_VERFN_DIGIT_ID || INSRC_CONTRACT_INT_ACCOUNT_ID
+    ) = '00117794544000001091';
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQUEST_CNCL (
+        REQUEST_SEQUENCE_ID,
+        INSURANCE_CONTRACT_ENTITY_ID,
+        INSURANCE_CONTRACT_BRANCH_ID,
+        INSRC_CONTRACT_INT_ACCOUNT_ID,
+        CONTRACT_FIRST_VERFN_DIGIT_ID,
+        CONTRACT_SECOND_VERFN_DIGIT_ID,
+        CHANNEL_ID,
+        CANCEL_BRANCH_ID,
+        REQUEST_CNCL_POLICY_DATE,
+        INSURANCE_PRODUCT_ID,
+        INSURANCE_MODALITY_TYPE,
+        PAYMENT_FREQUENCY_ID,
+        POLICY_ID,
+        COLECTIVE_CERTIFICATE_ID,
+        PREMIUM_AMOUNT,
+        PREMIUM_CURRENCY_ID,
+        CONTACT_EMAIL_DESC,
+        CUSTOMER_PHONE_DESC,
+        CUSTOMER_ID,
+        POLICY_ANNULATION_DATE,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE,
+        INSURANCE_CNCL_RETURNED_AMOUNT,
+        RET_AMOUNT_CURRENCY_ID,
+        INSRC_COMPANY_RETURNED_AMOUNT,
+        INSRC_CO_RTURN_AMOUNT_CCY_ID,
+        BBVA_RETURNED_AMOUNT,
+        BBVA_RTURN_AMOUNT_CURNCY_ID,
+        RL_ACCOUNT_ID,
+        REQUEST_STATUS_NAME,
+        REQUEST_TYPE
+    )
+VALUES
+    (
+        '2',
+        '0011',
+        '7794',
+        '4000001091',
+        '5',
+        '4',
+        'PC',
+        null,
+        TO_TIMESTAMP('2024-03-05', 'YYYY-MM-DD'),
+        '12',
+        null,
+        null,
+        '1300034100',
+        null,
+        null,
+        null,
+        'jose.sandoval.tirado.contractor@bbva.com',
+        '123456789',
+        '97788994',
+        TO_DATE('2024-03-05', 'YYYY-MM-DD'),
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        '4040123409876',
+        'APPLICATION_DATE',
+        '000'
+    );
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQ_CNCL_MOV (
+        REQUEST_SEQUENCE_ID,
+        SEQ_MOV_NUMBER,
+        ADDITIONAL_DATA_DESC,
+        CONTRACT_STATUS_DATE,
+        CONTRACT_STATUS_ID,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE
+    )
+VALUES
+    (
+        '2',
+        1,
+        '10',
+        TO_DATE('2024-03-05', 'YYYY-MM-DD'),
+        '01',
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP
+    );
+
+-- PEB
+UPDATE
+    PISD.T_PISD_INSURANCE_CONTRACT
+SET
+    CONTRACT_STATUS_ID = 'PEB'
+WHERE
+    (
+        INSURANCE_CONTRACT_ENTITY_Id || INSURANCE_CONTRACT_BRANCH_ID || CONTRACT_FIRST_VERFN_DIGIT_ID || CONTRACT_SECOND_VERFN_DIGIT_ID || INSRC_CONTRACT_INT_ACCOUNT_ID
+    ) = '40030789774580132200';
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQUEST_CNCL (
+        REQUEST_SEQUENCE_ID,
+        INSURANCE_CONTRACT_ENTITY_ID,
+        INSURANCE_CONTRACT_BRANCH_ID,
+        INSRC_CONTRACT_INT_ACCOUNT_ID,
+        CONTRACT_FIRST_VERFN_DIGIT_ID,
+        CONTRACT_SECOND_VERFN_DIGIT_ID,
+        CHANNEL_ID,
+        CANCEL_BRANCH_ID,
+        REQUEST_CNCL_POLICY_DATE,
+        INSURANCE_PRODUCT_ID,
+        INSURANCE_MODALITY_TYPE,
+        PAYMENT_FREQUENCY_ID,
+        POLICY_ID,
+        COLECTIVE_CERTIFICATE_ID,
+        PREMIUM_AMOUNT,
+        PREMIUM_CURRENCY_ID,
+        CONTACT_EMAIL_DESC,
+        CUSTOMER_PHONE_DESC,
+        CUSTOMER_ID,
+        POLICY_ANNULATION_DATE,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE,
+        INSURANCE_CNCL_RETURNED_AMOUNT,
+        RET_AMOUNT_CURRENCY_ID,
+        INSRC_COMPANY_RETURNED_AMOUNT,
+        INSRC_CO_RTURN_AMOUNT_CCY_ID,
+        BBVA_RETURNED_AMOUNT,
+        BBVA_RTURN_AMOUNT_CURNCY_ID,
+        RL_ACCOUNT_ID,
+        REQUEST_STATUS_NAME,
+        REQUEST_TYPE
+    )
+VALUES
+    (
+        '3',
+        '4003',
+        '0789',
+        '4580132200',
+        '7',
+        '7',
+        'PC',
+        null,
+        TO_TIMESTAMP('2024-03-05', 'YYYY-MM-DD'),
+        '12',
+        null,
+        null,
+        '1300034100',
+        null,
+        null,
+        null,
+        'jose.sandoval.tirado.contractor@bbva.com',
+        '123456789',
+        '97788994',
+        TO_DATE('2024-03-05', 'YYYY-MM-DD'),
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        '4040123409876',
+        'END_OF_VALIDATY',
+        '000'
+    );
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQ_CNCL_MOV (
+        REQUEST_SEQUENCE_ID,
+        SEQ_MOV_NUMBER,
+        ADDITIONAL_DATA_DESC,
+        CONTRACT_STATUS_DATE,
+        CONTRACT_STATUS_ID,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE
+    )
+VALUES
+    (
+        '3',
+        1,
+        '10',
+        TO_DATE('2024-03-05', 'YYYY-MM-DD'),
+        '01',
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP
+    );
+
+-- End of validaty, request one day ago
+UPDATE
+    PISD.T_PISD_INSURANCE_CONTRACT
+SET
+    CONTRACT_STATUS_ID = 'PEN'
+WHERE
+    (
+        INSURANCE_CONTRACT_ENTITY_Id || INSURANCE_CONTRACT_BRANCH_ID || CONTRACT_FIRST_VERFN_DIGIT_ID || CONTRACT_SECOND_VERFN_DIGIT_ID || INSRC_CONTRACT_INT_ACCOUNT_ID
+    ) = '40030789774580132100';
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQUEST_CNCL (
+        REQUEST_SEQUENCE_ID,
+        INSURANCE_CONTRACT_ENTITY_ID,
+        INSURANCE_CONTRACT_BRANCH_ID,
+        INSRC_CONTRACT_INT_ACCOUNT_ID,
+        CONTRACT_FIRST_VERFN_DIGIT_ID,
+        CONTRACT_SECOND_VERFN_DIGIT_ID,
+        CHANNEL_ID,
+        CANCEL_BRANCH_ID,
+        REQUEST_CNCL_POLICY_DATE,
+        INSURANCE_PRODUCT_ID,
+        INSURANCE_MODALITY_TYPE,
+        PAYMENT_FREQUENCY_ID,
+        POLICY_ID,
+        COLECTIVE_CERTIFICATE_ID,
+        PREMIUM_AMOUNT,
+        PREMIUM_CURRENCY_ID,
+        CONTACT_EMAIL_DESC,
+        CUSTOMER_PHONE_DESC,
+        CUSTOMER_ID,
+        POLICY_ANNULATION_DATE,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE,
+        INSURANCE_CNCL_RETURNED_AMOUNT,
+        RET_AMOUNT_CURRENCY_ID,
+        INSRC_COMPANY_RETURNED_AMOUNT,
+        INSRC_CO_RTURN_AMOUNT_CCY_ID,
+        BBVA_RETURNED_AMOUNT,
+        BBVA_RTURN_AMOUNT_CURNCY_ID,
+        RL_ACCOUNT_ID,
+        REQUEST_STATUS_NAME,
+        REQUEST_TYPE
+    )
+VALUES
+    (
+        '4',
+        '4003',
+        '0789',
+        '4580132100',
+        '7',
+        '7',
+        'PC',
+        null,
+        TO_TIMESTAMP('2024-03-04', 'YYYY-MM-DD'),
+        '12',
+        null,
+        null,
+        '1300034100',
+        null,
+        null,
+        null,
+        'jose.sandoval.tirado.contractor@bbva.com',
+        '123456789',
+        '97788994',
+        TO_DATE('2024-03-05', 'YYYY-MM-DD'),
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        '4040123409876',
+        'END_OF_VALIDATY',
+        '000'
+    );
+
+INSERT INTO
+    PISD.T_PISD_INSURANCE_REQ_CNCL_MOV (
+        REQUEST_SEQUENCE_ID,
+        SEQ_MOV_NUMBER,
+        ADDITIONAL_DATA_DESC,
+        CONTRACT_STATUS_DATE,
+        CONTRACT_STATUS_ID,
+        CREATION_USER_ID,
+        CREATION_DATE,
+        USER_AUDIT_ID,
+        AUDIT_DATE
+    )
+VALUES
+    (
+        '4',
+        1,
+        '10',
+        TO_DATE('2024-03-05', 'YYYY-MM-DD'),
+        '01',
+        'P121328',
+        SYSTIMESTAMP,
+        'P121328',
+        SYSTIMESTAMP
+    );
